df8e318548afbbdd8a85a788705abf849a176344
Josefu-Zero <andyjr2003@yahoo.com>
2026-07-07 22:51:56 -0500

    Name fix

--- src/main/java/com/coltis/ecommerce/BackendEcommerceApplication.java
+++ src/main/java/com/coltis/ecommerce/BackendEcommerceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
  
 @SpringBootApplication
-public class BackendEcommerceApplication {
-
+public class BackEndEcommerceApplication {
+ 
     public static void main(String[] args) {
-        SpringApplication.run(BackendEcommerceApplication.class, args);
+        SpringApplication.run(BackEndEcommerceApplication.class, args);
     }
 }
